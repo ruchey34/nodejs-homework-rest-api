bb612820dcba110f4fa349c4ae48b1a80bb49a9c
ruchey34 <me.oleg1@gmail.com>
2023-06-16 13:01:06 +0300

    hw#4 done

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,11 +1,22 @@
 const HttpError = require("../helpers/HttpError");
-
 const ctrlWrapper = require("../decorators/ctrlWrapper");
 const Contact = require("../models/contact-model");
 
 const getAllContacts = async (req, res) => {
-  const result = await Contact.find();
-  res.status(200).json(result);
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
+
+  let favoriteList;
+
+  if (favorite) {
+    favoriteList = result.filter((item) => item.favorite === true);
+  }
+  res.status(200).json(favorite ? favoriteList : result);
 };
 
 const getContactById = async (req, res) => {
@@ -18,7 +29,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
@@ -35,6 +47,7 @@ const updateContactById = async (req, res) => {
 const deleteContactById = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndRemove({ _id: contactId });
+
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -43,7 +56,6 @@ const deleteContactById = async (req, res) => {
     message: "contact deleted",
   });
 };
-
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
@@ -54,7 +66,6 @@ const updateStatusContact = async (req, res) => {
 
   res.json(result);
 };
-
 
 module.exports = {
   getAllContacts: ctrlWrapper(getAllContacts),
--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_URI='mongodb+srv://shora:QWEasdzx30@cluster0.3aszojj.mongodb.net/db-contacts?retryWrites=true&w=majority';
-const PORT= 3000;
-
-mongoose.set("strictQuery", true);
-
-mongoose.connect(DB_URI)
+mongoose
+  .connect(process.env.DB_URI)
   .then(() => {
-    app.listen(PORT);
+    app.listen(process.env.PORT || 3000);
     console.log("Database connection successful");
   })
-  .catch(error => {
-    console.log(error.message)
+  .catch((error) => {
+    console.log(error.message);
     process.exit(1);
   });
